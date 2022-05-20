b5f19f74aa3edc487df2b516292a0f5ce562b898
Aleš Sedláček <xsedla11@fi.muni.cz>
2022-05-20 19:03:47 +0200

    Storing payments from excel to variable

--- Expense Manager/Excel/Import.cs
+++ Expense Manager/Excel/Import.cs
@@ -5,7 +5,7 @@ namespace Expense_Manager.Excel
 {
     internal static class Import
     {
-        public static async Task FromXlsx()
+        public static void FromXlsx()
         {
             var filePath = GetImportPath();
             if (filePath == string.Empty)
@@ -43,6 +43,7 @@ namespace Expense_Manager.Excel
 
         private static List<Payment> GetDataFromXlsx(XLWorkbook workBook)
         {
+            var result = new List<Payment>();
             IXLWorksheet workSheet = workBook.Worksheet(1);
 
             bool firstRow = true;
@@ -58,11 +59,17 @@ namespace Expense_Manager.Excel
                     var dateCreated = DateTime.Parse(row.Cell("B").Value.ToString());
                     var amount = Convert.ToDouble(row.Cell("C").Value);
 
-                    // TODO:
+                    result.Add(new Payment
+                    {
+                        IsIncome = isIncome,
+                        DateCreated = dateCreated,
+                        Amount = amount,
+                        UserId = Globals.User.Id
+                    });
                 }
             }
 
-            return new List<Payment>();
+            return result;
         }
     }
 }
